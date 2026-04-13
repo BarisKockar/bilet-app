3e591676e32345e2a370b22c378ca44834478135
barış <baris06kockar@gmail.com>
2026-04-13 23:21:11 +0300

    fix qr to use production payment url

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -630,20 +630,18 @@ export default function Page() {
     ? selectedSeats.map((s) => s.seat_code).join(", ")
     : selectedSeat?.seat_code || "-";
 
-  const qrValue =
-  typeof window !== "undefined"
-    ? `${window.location.origin}/odeme?bank=${encodeURIComponent(
-        ibanInfo.bank_name
-      )}&receiver=${encodeURIComponent(
-        ibanInfo.iban_name
-      )}&iban=${encodeURIComponent(
-        ibanInfo.iban_number
-      )}&desc=${encodeURIComponent(
-        paymentDescription
-      )}&amount=${encodeURIComponent(
-        amount || ""
-      )}`
-    : "";
+  const PUBLIC_BASE_URL = "https://bilet-app-863n.vercel.app/"; // kendi canlı domainin neyse onu yaz
+
+  const qrValue = `${PUBLIC_BASE_URL}/odeme?bank=${encodeURIComponent(
+    ibanInfo.bank_name
+  )}&receiver=${encodeURIComponent(
+    ibanInfo.iban_name
+  )}&iban=${encodeURIComponent(
+    ibanInfo.iban_number
+  )}&desc=${encodeURIComponent(
+    paymentDescription
+  )}&amount=${encodeURIComponent(amount || "")}`;
+
   return (
     <main
       style={{
